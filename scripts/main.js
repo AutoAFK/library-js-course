e01a667621434ccac145f32803f9ce2fa0159904
AutoAFK <ranhaim85@gmail.com>
2026-04-02 00:43:18 +0300

    Read and Remove buttons
    Added an option to remove books from the
    library. On top of that did a little refactor
    of functions and optimizations to the dom.

--- scripts/main.js
+++ scripts/main.js
@@ -16,19 +16,27 @@ function Book(name, author, numberOfPages) {
   this.name = name;
   this.author = author;
   this.numberOfPages = numberOfPages;
+  this.read = false;
 
   this.details = () =>
     `${this.name}, ${this.author}, ${this.numberOfPages} pages`;
+
+  this.toggleRead = () => {
+    this.read = !this.read;
+  };
 }
 
 function addBookToLibrary(name, author, numberOfPages) {
   const book = new Book(name, author, numberOfPages);
   library.push(book);
+
+  refreshBooksList();
 }
 
 function displayBooks() {
   for (const book of library) {
     const bookDOM = document.createElement("li");
+    bookDOM.dataset.id = book.id;
     bookDOM.classList.add("book");
 
     const bookDetailsParagraph = document.createElement("p");
@@ -37,15 +45,49 @@ function displayBooks() {
 
     bookDOM.appendChild(bookDetailsParagraph);
 
+    const divButtons = document.createElement("div");
+
+    const toggleReadButton = document.createElement("button");
+    toggleReadButton.textContent = "Read";
+    toggleReadButton.classList.add("btn-read");
+
+    divButtons.appendChild(toggleReadButton);
+
     const removeButton = document.createElement("button");
-    removeButton.dataset.id = book.id;
     removeButton.textContent = "Remove";
     removeButton.classList.add("btn-remove");
 
-    bookDOM.appendChild(removeButton);
+    divButtons.appendChild(removeButton);
+
+    bookDOM.appendChild(divButtons);
 
     booksListDOM.appendChild(bookDOM);
   }
+}
+
+function refreshBooksList() {
+  booksListDOM.innerHTML = "";
+  displayBooks();
+}
+
+function toggleBookRead(id) {
+  for (const book of library) {
+    if (book.id === id) {
+      book.toogleRead();
+      return;
+    }
+  }
+}
+
+function removeBookFromLibrary(id) {
+  let index = 0;
+  for (let length = library.length; index < length; index++) {
+    if (library[index].id === id) {
+      break;
+    }
+  }
+  library.splice(index, 1);
+  refreshBooksList();
 }
 
 addBookButtonDOM.addEventListener("click", () => {
@@ -54,27 +96,17 @@ addBookButtonDOM.addEventListener("click", () => {
   const bookPagesAmount = dialogBookPagesAmount.value;
 
   addBookToLibrary(bookName, bookAuthor, bookPagesAmount);
-  booksListDOM.innerHTML = "";
-  displayBooks();
 });
 
 booksListDOM.addEventListener("click", (event) => {
   const target = event.target;
+  const bookID = target.parentNode.dataset.id;
 
-  if (!target.classList.contains("btn-remove")) {
-    return;
+  if (target.classList.contains("btn-remove")) {
+    removeBookFromLibrary(bookID);
+  } else if (target.classList.contains("btn-read")) {
+    toggleBookRead(bookID);
   }
-
-  let index = 0;
-  for (let length = library.length; index < length; index++) {
-    if (library[index].id === target.dataset.id) {
-      break;
-    }
-  }
-  library.splice(index, 1);
-
-  booksListDOM.innerHTML = "";
-  displayBooks();
 });
 
 addBookToLibrary("Harry potter", "JK Rolling", 290);
