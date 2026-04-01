34e9aa7e8cdad74ace92ea4c5980b93cca991596
AutoAFK <ranhaim85@gmail.com>
2026-04-01 06:02:50 +0300

    ADDED: add and remove book from the list

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 const booksListDOM = document.querySelector("#books-list");
+const addBookButtonDOM = document.querySelector("#add-button");
+
+const dialogBookName = document.querySelector("#book-name");
+const dialogBookAuthor = document.querySelector("#book-author");
+const dialogBookPagesAmount = document.querySelector("#book-pages-amount");
+
 const library = [];
 
 function Book(name, author, numberOfPages) {
@@ -10,8 +16,9 @@ function Book(name, author, numberOfPages) {
   this.name = name;
   this.author = author;
   this.numberOfPages = numberOfPages;
-  
-  this.details = () => `${this.name}, ${this.author}, ${this.numberOfPages} pages`;
+
+  this.details = () =>
+    `${this.name}, ${this.author}, ${this.numberOfPages} pages`;
 }
 
 function addBookToLibrary(name, author, numberOfPages) {
@@ -30,9 +37,45 @@ function displayBooks() {
 
     bookDOM.appendChild(bookDetailsParagraph);
 
+    const removeButton = document.createElement("button");
+    removeButton.dataset.id = book.id;
+    removeButton.textContent = "Remove";
+    removeButton.classList.add("btn-remove");
+
+    bookDOM.appendChild(removeButton);
+
     booksListDOM.appendChild(bookDOM);
   }
 }
+
+addBookButtonDOM.addEventListener("click", () => {
+  const bookName = dialogBookName.value;
+  const bookAuthor = dialogBookAuthor.value;
+  const bookPagesAmount = dialogBookPagesAmount.value;
+
+  addBookToLibrary(bookName, bookAuthor, bookPagesAmount);
+  booksListDOM.innerHTML = "";
+  displayBooks();
+});
+
+booksListDOM.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (!target.classList.contains("btn-remove")) {
+    return;
+  }
+
+  let index = 0;
+  for (let length = library.length; index < length; index++) {
+    if (library[index].id === target.dataset.id) {
+      break;
+    }
+  }
+  library.splice(index, 1);
+
+  booksListDOM.innerHTML = "";
+  displayBooks();
+});
 
 addBookToLibrary("Harry potter", "JK Rolling", 290);
 addBookToLibrary("Court of Thorns and Roses", "Shara J. Maas", 500);
